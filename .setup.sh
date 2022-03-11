3f81b6cf737d36e7afbcd309d8b4e93b520b301b
eduuh <edwinkamaumuraya0@outlook.com>
2022-03-11 10:06:34 +0300

    Updated the configuration for git

--- .setup.sh
+++ .setup.sh
@@ -3,5 +3,24 @@
 # set up my environment
 # creating a hard link to my hosts.config file
 
+git init --bare $HOME/.dotfiles  
+alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+dotfiles remote add origin https://github.com/eduuh/dotfiles.git
+dotfiles branch --set-upstream-to=origin/main main
+
+## delete .config:  cache : delete if successful
+mv ~/.config ~/.config.bak
+mv ~/.local/bin ~/.local/bin.bak
+mv .gitconfig .gitconfig.bak
+mv .tmux.conf .tmux.conf
+mv .xprofile  .xprofile.bak
+mv ~/.vsnip/ ~/.vsnip.bak
+
+dotfiles checkout
+
+echo ".dotfiles" >> .gitignore
+
+dotfiles config --local status.showUntrackedFiles no
+
 sudo mv /etc/hosts /etc/hosts.bak
 sudo ln .config/hosts /etc/hosts
